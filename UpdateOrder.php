4c3032e7433a71376235b5cef6363083277d89b6
Joanabernz <124427180+Joanabernz@users.noreply.github.com>
2025-10-28 20:24:46 +0800

    Refactor database connection in UpdateOrder.php
    dd

--- UpdateOrder.php
+++ UpdateOrder.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "electstore");
+$conn = new mysqli ('yamanote.proxy.rlwy.net', 'root', 'jUpvuIHgXOAUuquWOoJVXMpYQmGiOkyU', 'electstore');
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }
@@ -22,4 +22,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 $conn->close();
+
 ?>
